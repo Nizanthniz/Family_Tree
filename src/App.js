a8f68ea80a3002df50f8d2360547a928ff85abb5
nizanth-iteron <nizanth@iteron.in>
2022-08-08 13:32:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from "./main";
+import Mytree from "./mytree";
+import React from "react";
+import Sample from "./sample";
+
+
+
+function App(){
+  return(
+    <>
+    <Router>
+      <Routes>
+        <Route path="/sample1" element={<Sample/>} />
+        <Route path="/mytree" element={<Mytree/>} />
+        <Route path="/main/:id/:user_id" element={<Main/>} />
+        
+       
+      </Routes>
+    </Router>
+
+    </>
+    );
+
 }
-
 export default App;
